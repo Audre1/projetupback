0e0190a34e2d707d4bbbfa33ffdd96da7b9cdb32
AWINAMEL <nardagnimel.digifemmes@gmail.com>
2024-11-23 00:27:41 +0000

    j'ai modifie  la structure de admin et medecin dans le dossier model

--- models/admin.go
+++ models/admin.go
@@ -1,7 +1,12 @@
 package models
 
+// Import de la bibliothèque GORM pour la gestion des bases de données
+
+// Définition du modèle Admin
 type Admin struct {
-	Utilisateur
+	ID       uint   `json:"id"`                  // Identifiant unique de l'administrateur (clé primaire)
+	Email    string `json:"email" gorm:"unique"` // Email de l'administrateur, doit être unique dans la base de données
+	Password string `json:"password"`            // Mot de passe de l'administrateur
 }
 
 // TableName permet de définir le nom de la table en base de données
--- models/medecin.go
+++ models/medecin.go
@@ -1,9 +1,11 @@
 package models
 
+// Medicin représente un médecin specialiste travaillant dans la clinique
 type Medecin struct {
 	Utilisateur
-	Specialite     string         `json:"specialite" gorm:"not null"`
-	Disponibilites []PlageHoraire `json:"disponibilites" gorm:"foreignKey:MedecinID"`
+	ID                int    `json:"id"`                 // Identifiant unique du médecin
+	Speciality        string `json:"speciality"`         // Spécialité du médecin (par ex., cardiologie)
+	ConsultationHours string `json:"consultation_hours"` // Horaires de consultation
 }
 
 // TableName permet de définir le nom de la table en base de données
